618bc2e5383262ff04e72ec712575a0d903e82bc
Navin Pranav <navin.pranav@trustrace.com>
2022-02-11 13:44:44 +0530

    Added bootstrap

--- todo/src/app/server/server.component.ts
+++ todo/src/app/server/server.component.ts
@@ -3,12 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-server',
     templateUrl: './server.component.html',
-    styles: [`
-    .online {
-        color: white;
-        }
-    `]
+    styleUrls: ['./server.component.css']
 })
+
 export class ServerComponent{
     serverId: number = 10;
     serverStatus: string = 'Offline';
